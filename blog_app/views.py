73ded9b92ad7aedb2b982f76ecfe34deb0e79da1
doctorchebo <marcelo.munoz.coaquira@gmail.com>
2023-05-30 20:57:28 -0400

    Adding views, urls and html files

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+# blog_app/views.py
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post, Comment
+from .forms import CommentForm
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ('title', 'content')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ('title', 'content')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('blog_app:post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+    else:
+        form = CommentForm()
+    return render(request, 'add_comment_to_post.html', {'form': form})
