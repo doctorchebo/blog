b0c9250f834f45b948b811221e965d7579c9fc00
doctorchebo <marcelo.munoz.coaquira@gmail.com>
2023-05-31 21:27:06 -0400

    Adding blog post card and some styling

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+    @property
+    def reading_time(self):
+        words_per_minute = 180 # The average reading speed is between 180 and 200 words per minute
+        return int(len(self.content.split()) / words_per_minute)
+
+    def get_absolute_url(self):
+        return reverse('blog_app:post_detail', args=[self.id])
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
