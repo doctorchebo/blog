6d1a50e73c57bb2d099f727293d9b40c7e21257d
doctorchebo <marcelo.munoz.coaquira@gmail.com>
2023-07-14 15:43:13 -0400

    Adding dev and prod config and adding favicon

--- blog/settings.py
+++ blog/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = bool(int(os.getenv('DEBUG', '0')))
 
-ALLOWED_HOSTS = ['escapalamatrix.com', 'www.escapalamatrix.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
