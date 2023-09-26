6139591ff983b255f3baf1ab538b0d60f72c7d92
doctorchebo <marcelo.munoz.coaquira@gmail.com>
2023-09-25 20:59:26 -0400

    Adding like btn for not logged users

--- blog_app/static/js/like.js
+++ blog_app/static/js/like.js
@@ -1,23 +1,26 @@
 // like button
 $(document).ready(function () {
-    function getCookie(name) {
-      var value = "; " + document.cookie;
-      var parts = value.split("; " + name + "=");
-      if (parts.length == 2) return parts.pop().split(";").shift();
-    }
-    $.ajaxSetup({
-      beforeSend: function (xhr, settings) {
-        if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
-          xhr.setRequestHeader("X-CSRFToken", $("input[name=csrfmiddlewaretoken]").val());
-        }
-      },
-    });
-    $("body").on("click", ".like-button", function () {
-      var $button = $(this);
-      var postId = $button.data("post-id");
-      var $countSpan = $button.next(".like-count"); // adjust this line
-      var currentLikes = parseInt($countSpan.text(), 10) || 0; // if NaN, default to 0
+  function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+  }
+  $.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+      if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", $("input[name=csrfmiddlewaretoken]").val());
+      }
+    },
+  });
 
+  $("body").on("click", ".like-button", function () {
+    var $button = $(this);
+    var postId = $button.data("post-id");
+    var $countSpan = $button.next(".like-count");
+    var currentLikes = parseInt($countSpan.text(), 10) || 0;
+    // Check if the user is authenticated
+    if (userIsAuthenticated) {
+      // User is authenticated, perform the like action
       $.ajax({
         url: `/like_post/${postId}/`,
         type: "POST",
@@ -36,5 +39,21 @@ $(document).ready(function () {
           }
         },
       });
-    });
+    } else {
+      console.log("show popup");
+      // User is not authenticated, show the popup
+      showLikePopup();
+    }
   });
+  // Function to show the like popup
+  function showLikePopup() {
+    // Find the like popup element by its class and make it visible
+    var likePopup = document.getElementById("like-popup");
+    likePopup.style.display = "block";
+    var closeButton = document.getElementById("close-like-popup");
+    closeButton.onclick = function () {
+      console.log("clicked");
+      likePopup.style.display = "none";
+    };
+  }
+});
