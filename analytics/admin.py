ae75beb9011f0ff18b42212b499c93bef7f4bb8b
doctorchebo <marcelo.munoz.coaquira@gmail.com>
2024-04-09 10:39:54 -0400

    adding migration files

--- analytics/admin.py
+++ analytics/admin.py
@@ -6,5 +6,3 @@ class PageVisitAdmin(admin.ModelAdmin):
     list_display = ('url', 'visit_date', 'duration_in_seconds', 'user', 'ip_address')
     list_filter = ('url', 'visit_date', 'user', 'ip_address')
 
-    
-
